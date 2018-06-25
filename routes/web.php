bba220b7907a90b45b9ab9ec0984179c27c62af4
Alisson Vieira <alisson.echo@gmail.com>
2018-06-25 14:31:39 -0300

    Criando login administrativo

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,16 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group([
     'prefix' => 'admin',
     'as' => 'admin.',
-    'middleware' => 'can:admin'
+    'namespace' => 'Admin\\'
 ], function(){
-    Route::get('/', function () {
-        return "Área administrativa";
+    Route::name('login')->get('login', 'Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login');
+
+    Route::group(['middleware' => 'can:admin'], function(){
+        Route::name('logout')->post('logout', 'Auth\LoginController@logout');
+        Route::get('dashboard', function(){
+            return "Área administrativa funcionando";
+        });
     });
 });
 
